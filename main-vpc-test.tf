84a24239f6fabbfc4102d9035b78f290cc4afcd2
Jihyun <ibaikji@gmail.com>
2022-03-22 14:05:32 +0900

    Test

--- main-vpc-test.tf
+++ main-vpc-test.tf
@@ -1,15 +1,15 @@
-#### VPC ####
+# #### VPC ####
 
-resource "aws_vpc" "vpc-test-jenkins" {
-  cidr_block  = "10.181.130.0/24" # 업뎃 필요
-  enable_dns_hostnames = true
+# resource "aws_vpc" "vpc-test-jenkins" {
+#   cidr_block  = "10.181.130.0/24" # 업뎃 필요
+#   enable_dns_hostnames = true
 
-  tags = {
-    Name = "vpc-test-jenkins"
-  }
-}
-
-# resource "aws_vpc_ipv4_cidr_block_association" "vpc-test-secondary-cidr-1" { 
-#   vpc_id     = aws_vpc.vpc-test.id
-#   cidr_block = "100.64.0.0/16" 
+#   tags = {
+#     Name = "vpc-test-jenkins"
+#   }
 # }
+
+# # resource "aws_vpc_ipv4_cidr_block_association" "vpc-test-secondary-cidr-1" { 
+# #   vpc_id     = aws_vpc.vpc-test.id
+# #   cidr_block = "100.64.0.0/16" 
+# # }
