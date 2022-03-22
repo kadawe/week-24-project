725910d78775e3c79b98192106bd9b5e0984e64c
Jihyun <ibaikji@gmail.com>
2022-03-22 13:19:44 +0900

    Test

--- compute/variables.tf
+++ compute/variables.tf
@@ -1,12 +1,12 @@
-# --- comput/variables.tf ---
+# # --- comput/variables.tf ---
 
-variable "web_sg" {}
-variable "public_subnet" {}
+# variable "web_sg" {}
+# variable "public_subnet" {}
 
 
 
-variable "web_instance_type" {
-  type    = string
-  default = "t2.micro"
-}
+# variable "web_instance_type" {
+#   type    = string
+#   default = "t2.micro"
+# }
 
