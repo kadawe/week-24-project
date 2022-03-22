5c4c9e375728e5268ed6a08a83bbb64cf24d077f
Jihyun <ibaikji@gmail.com>
2022-03-22 10:48:41 +0900

    Test

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+region = "ap-northeast-2"
+id-vpc-share = "vpc-0539408952ee3f107"
+id-vpc-share-pub1 = "subnet-00fe1fca536358420"
+
+## test ##
+id-ip-eni-ec2-test-vm-amzn2 = "10.181.121.80"
---
+++ variable.tf
@@ -0,0 +1,16 @@
+
+
+variable "region" {
+    type = string
+}
+variable "id-vpc-share" {
+    type = string
+}
+
+variable "id-vpc-share-pub1" {
+    type = string
+}
+
+variable "id-ip-eni-ec2-test-vm-amzn2" {
+    type = string
+}
